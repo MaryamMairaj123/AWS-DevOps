b5e33d3799e81f5d01345c7e12b33e9530bc9226
MaryamMairaj123 <36111301+MaryamMairaj123@users.noreply.github.com>
2024-10-17 16:38:02 +0400

    Create start_container.sh

---
+++ scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+#docker pull maryammairaj/python-flask-app-service:latest
+docker pull maryammairaj/python-flask-app-service
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 maryammairaj/python-flask-app-service
