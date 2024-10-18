dc717f58ec6e6b5670ac949d779f23138bb547b0
MaryamMairaj123 <36111301+MaryamMairaj123@users.noreply.github.com>
2024-10-18 12:50:37 +0400

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,3 +3,16 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
+# #!/bin/bash
+# set -e
+
+# Stop the running container (if any)
+container_name="friendly_allen"
+
+if [ $(docker ps -q -f name=$container_name) ]; then
+    echo "Stopping the container: $container_name"
+    docker stop $container_name
+    docker rm $container_name
+else
+    echo "No running container found with name: $container_name"
+fi
