28b6b46385371d7b7a6316c237903000f84126b2
MaryamMairaj123 <36111301+MaryamMairaj123@users.noreply.github.com>
2024-11-02 00:12:04 +0400

    Update provider.tf

--- aws-devops-main/day-24/provider.tf
+++ aws-devops-main/day-24/provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.11.0"
+      source = "hashicorp/aws"
+      version = "5.73.0"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
-  region = "us-east-1"
+  region = "us-west-1"
 }
